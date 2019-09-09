9eeb4daf847688597a0946ed3679cb3b0fe3d5c4
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2019-09-09 19:59:50 -0300

    Estilização do login e do cadastro

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/img/logo.svg';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
