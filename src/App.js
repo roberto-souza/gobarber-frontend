1fd3c70185d403765388c33e2c821622b94d316f
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2019-09-09 19:32:01 -0300

    Adicionando um global style para a aplicação

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import './dependencies';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
