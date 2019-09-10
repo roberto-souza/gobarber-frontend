17dbc20a1683045b4d28007a6b4ad83cc58ff0cc
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2019-09-10 20:06:36 -0300

    Adição das notificações no sistema, podendo marcar como lida

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '../Notifications';
+
 import logo from '~/assets/img/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -13,16 +15,19 @@ export default function Header() {
           <img src={logo} alt="GoBarber" />
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
-        <Profile>
-          <div>
-            <strong>Roberto Souza</strong>
-            <Link to="/profile">Meu Perfil</Link>
-          </div>
-          <img
-            src="https://api.adorable.io/avatars/50/abott@adorable.png"
-            alt="Roberto Souza"
-          />
-        </Profile>
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Roberto Souza</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Roberto Souza"
+            />
+          </Profile>
+        </aside>
       </Content>
     </Container>
   );
--- src/dependencies.js
+++ src/dependencies.js
@@ -1,2 +1,3 @@
 import './config/ReactotronConfig';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
