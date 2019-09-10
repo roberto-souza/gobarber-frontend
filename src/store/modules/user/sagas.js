78b5f8768d84b6842c54f2f2281cdf7f2794883c
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2019-09-10 20:53:30 -0300

    Colocando foto de perfil e atualizando dados do header

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -9,9 +9,14 @@ import UserTypes from './types';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
 
     const response = yield call(api.put, 'users', profile);
 
