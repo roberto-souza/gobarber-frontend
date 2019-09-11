72223cf8611c8b9612fdf6b21bc97534f086c5f0
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2019-09-10 21:10:13 -0300

    Estilização e criação dos components do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+// import api from '~/services/api';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
